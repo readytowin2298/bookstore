6ee2be7086ac75359e7c5cfe8035ea923d07a51f
David Ames ubnt desktop <readytowin2298@gmail.com>
2021-04-20 11:28:26 -0500

    finished project

--- config.js
+++ config.js
@@ -4,9 +4,9 @@
 let DB_URI = `postgresql://`;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
+  DB_URI = `${DB_URI}mvufztlp:ca2cGTbkqTdtd70h6811rVtw7Bq0CdQ6@queenie.db.elephantsql.com:5432/mvufztlp`;
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+  DB_URI = process.env.DATABASE_URL || `${DB_URI}wftaktqp:XagABaqcfGJ68VPx3LWHdcH0zHWCgQcf@queenie.db.elephantsql.com:5432/wftaktqp`;
 }
 
 
